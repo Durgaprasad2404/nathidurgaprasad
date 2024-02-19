1bbb2cdd5ab4003534f08ce8003df8edf8aa3334
dp <nathiprasad2404@gmail.com>
2024-02-19 14:45:08 +0530

    Dp

---
+++ script.js
@@ -0,0 +1,6 @@
+const projectElement = document.getElementById("projcetBtn");
+const projectContainer = document.getElementById("projects");
+
+projectElement.addEventListener("click", function () {
+  projectContainer.style.display = "flex";
+});
