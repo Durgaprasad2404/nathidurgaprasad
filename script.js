a1064691b9c862e59ba59dde1c8a4772045579a0
dp <nathiprasad2404@gmail.com>
2024-04-01 17:24:53 +0530

    Dp

--- script.js
+++ script.js
@@ -1,6 +1,1 @@
-const projectElement = document.getElementById("projcetBtn");
-const projectContainer = document.getElementById("projects");
 
-projectElement.addEventListener("click", function () {
-  projectContainer.style.display = "flex";
-});
