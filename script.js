3fadaba46cb4ba14d77904090cc33643fe68be7c
dp <nathiprasad2404@gmail.com>
2024-06-19 12:13:00 +0530

    form data to google sheets

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
+const scriptURL =
+  "https://script.google.com/macros/s/AKfycbxTZY9A3clfEhKXejrfmwV0AOkz1iA3R85_3VotuLBiee4yv65pomSwVEVU_hwfrSTM/exec";
 
+const form = document.forms["contactForm"];
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  fetch(scriptURL, { method: "POST", body: new FormData(form) })
+    .then((response) =>
+      alert("Thank you! your form is submitted successfully.")
+    )
+    .then(() => {
+      window.location.reload();
+    })
+    .catch((error) => console.error("Error!", error.message));
+});
